7c96d3e81f43f66991a40bf9d392bfa17b87d81d
Aviran Bachar <aviranbac1@gmail.com>
2022-12-09 01:19:37 +0200

    fix(navbar): fixing link so that App.js won't be rendered again. Also remove React.StrictMode in order to load requests once and not twice during development

--- frontend/src/components/ShopNavbar/ShopNavbar.js
+++ frontend/src/components/ShopNavbar/ShopNavbar.js
@@ -1,5 +1,7 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { useLocation } from "react-router";
+import "./ShopNavbar.css";
+import { Link } from "react-router-dom";
 
 const ShopNavbar = () => {
     const { pathname } = useLocation();
@@ -9,8 +11,8 @@ const ShopNavbar = () => {
             <Container className="justify-content-start m-0">
                 <Navbar.Brand>Items Shop</Navbar.Brand>
                 <Nav activeKey={pathname}>
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/cart">Cart</Nav.Link>
+                    <Nav.Link as={Link} to="/">Catalogue</Nav.Link>
+                    <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
